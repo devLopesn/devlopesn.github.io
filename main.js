f9b8f264c57bed3880ed4155cb0b806729e1967a
r0ger1 <72942827+r0ger1@users.noreply.github.com>
2024-08-17 20:54:44 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const botao = document.querySelector('#botao');
+
+
+
+botao.addEventListener('click', () =>
+{
+    window.scroll({top:window.innerHeight, behavior: "smooth"})
+})
+
+
+const bplanos = document.querySelector('#bplanos')
+
+bplanos.addEventListener('click', () => {
+
+    window.scroll({top:window.innerHeight, behavior: "smooth"})
+})
